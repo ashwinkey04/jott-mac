a650eee0f166a804963c754c5a0e0eeb0518cba5
Ashwin Ramakrishnan <ashwinkey04@gmail.com>
2022-01-27 19:30:36 +0530

    Initial Commit

---
+++ jott/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jott
+//
+//  Created by Ashwin Ramakrishnan on 27/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jott/jottApp.swift
@@ -0,0 +1,17 @@
+//
+//  jottApp.swift
+//  jott
+//
+//  Created by Ashwin Ramakrishnan on 27/01/22.
+//
+
+import SwiftUI
+
+@main
+struct jottApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
